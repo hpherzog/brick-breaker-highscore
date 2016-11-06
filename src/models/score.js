c6ac268a0d9229506f6f575012eec959f02c7417
Hans-Peter Herzog <hherzog@sipwise.com>
2016-11-07 00:06:02 +0100

    Improved validation for name

--- src/models/score.js
+++ src/models/score.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var $ = require('chai').assert;
 var Sequelize = require('sequelize');
 
@@ -17,17 +18,21 @@ module.exports.define = function define(options) {
 
         name: {
             type: Sequelize.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [3, 64]
+            },
+            set: function (name) {
+                this.setDataValue('name', _.trim(name));
+            },
         },
 
         value: {
             type: Sequelize.BIGINT,
             allowNull: false,
             validate: {
-                min: {
-                    args: 1,
-                    msg: 'min'
-                }
+                min: 0
             }
         },
 
@@ -35,10 +40,7 @@ module.exports.define = function define(options) {
             type: Sequelize.INTEGER,
             allowNull: false,
             validate: {
-                min: {
-                    args: 1,
-                    msg: 'min'
-                }
+                min: 1
             }
         }
     }, {
